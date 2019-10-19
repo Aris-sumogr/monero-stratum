39569b17c6d55828ff7229a4906b0f7a530c5ba8
Aris-sumogr <54616978+Aris-sumogr@users.noreply.github.com>
2019-10-19 15:07:04 +0300

    Update hashing.c

--- hashing/src/hashing.c
+++ hashing/src/hashing.c
@@ -2,7 +2,7 @@
 
 void cryptonight_hash(const char* input, char* output, uint32_t len, uint64_t height) {
     const int variant = input[0] >= 7 ? input[0] - 6 : 0;
-    cn_slow_hash(input, len, output, variant, 0, height);
+    cn_monero_slow_hash(input, len, output, variant, 0, height);
 }
 
 void cryptonight_fast_hash(const char* input, char* output, uint32_t len) {
